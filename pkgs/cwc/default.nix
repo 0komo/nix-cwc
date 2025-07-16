67598f54c050e9faf4fec932086fbc6516b032a4
Komo <afiqquraisyzulkarnain@gmail.com>
2025-07-17 03:39:48 +0800

    feat!: switch to nightly

--- pkgs/cwc/default.nix
+++ pkgs/cwc/default.nix
@@ -2,6 +2,8 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  # Options
+  withLuaEnv ? null,
   # Build-time dependencies
   git,
   gobject-introspection,
@@ -27,39 +29,50 @@
   xxHash,
   xorg,
   xwayland,
-}:
+}@attrs:
 
 let
-  luaEnv = luajit.withPackages (
+  _unused = [ withLuaEnv ];
+
+  inherit (builtins)
+    readFile
+    fromTOML
+    substring
+    elemAt
+    ;
+  luaEnv = attrs.withLuaEnv or luajit.withPackages (
     p: with p; [
       lgi
     ]
   );
+  latestCommit = elemAt (fromTOML (readFile ../../commit.toml)).commits 0;
+
+  mkLuaPath =
+    env: isNativeMod:
+    if isNativeMod then
+      "${env}/lib/lua/5.1/?.so;;"
+    else
+      "${env}/share/lua/5.1/?.lua;${env}/share/lua/5.1/?/init.lua;;";
 in
 stdenv.mkDerivation (self: {
-  pname = "cwc";
-  version = "0.1.0";
+  name = "cwc";
+  version = "0-unstable-${substring 0 6 latestCommit.rev}";
 
   src = fetchFromGitHub {
     owner = "Cudiph";
     repo = "cwcwm";
-    rev = "v${self.version}";
-    hash = "sha256-qkhlxk2t1cBs3VO6VwJicuhxUa12w+FTvVoCSLpWX1Q=";
-    leaveDotGit = true;
+    inherit (latestCommit)
+      rev
+      hash
+      ;
   };
-
-  patches = lib.pipe (builtins.readDir ./.) [
-    builtins.attrNames
-   (builtins.filter (s: lib.hasSuffix ".patch" s))
-   (map (s: ./${s} ))
-  ];
 
   nativeBuildInputs = [
     git
     gobject-introspection
+    makeWrapper
     meson
     ninja
-    makeWrapper
     pkg-config
     python3Packages.python
     wayland-protocols
@@ -71,47 +84,38 @@ stdenv.mkDerivation (self: {
     gdk-pixbuf
     hyprcursor
     libdrm
-    luajit
-    luaEnv
     libinput
     libxkbcommon
+    luajit
     pango
-    wlroots_0_19
     wayland
     wayland-scanner
-    xxHash
+    wlroots_0_19
     (with xorg; [
       libxcb
       xcbutilwm
     ])
     xwayland
+    xxHash
   ];
 
   mesonFlags = [
     "-Dplugins=true"
-    # fix missing drm_fourcc.h header
-    "-Dc_args=-I${libdrm.dev}/include/libdrm"
   ];
 
   mesonBuildType = "release";
 
-  postPatch = ''
-    substituteInPlace ./{,src,cwctl}/meson.build \
-      --replace-fail "/usr" "$out"
-
-    substituteInPlace ./src/luac.c \
-      --subst-var-by LUA_ENV "${luaEnv}"
-  '';
-
-  postConfigure = ''
-    rm -fr .git
-  '';
-
   preFixup = ''
     gappsWrapperArgs+=(
       --prefix LD_LIBRARY_PATH : "$out/share/cwc/plugins"
+      --prefix LUA_PATH ';' "${mkLuaPath self.passthru.luaEnv false}"
+      --prefix LUA_CPATH ';' "${mkLuaPath self.passthru.luaEnv true}"
     )
   '';
+
+  passthru = {
+    inherit luaEnv;
+  };
 
   meta.mainProgram = "cwc";
 })
